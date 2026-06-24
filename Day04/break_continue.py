fda89358ea4e2d79fd4ea2ee34a220f05130064a
Eiman Ajmal <khaneiman36@gmail.com>
2026-06-24 16:36:36 +0500

    Completed Day 4 - Loops

---
+++ Day04/break_continue.py
@@ -0,0 +1,14 @@
+for i in range(1, 11):
+
+    if i == 5:
+        break
+
+    print(i)
+
+print("Loop Ended")
+for i in range(1, 11):
+
+    if i == 5:
+        continue
+
+    print(i)
---
+++ Day04/sum_while.py
@@ -0,0 +1,8 @@
+total = 0
+num = 1
+
+while num <= 100:
+    total = total + num
+    num = num + 1
+
+print("Sum =", total)
