d4c92c4a6a7c8b6402225d7211ab473d5ca3cd87
Eiman Ajmal <khaneiman36@gmail.com>
2026-06-25 19:46:23 +0500

    Completed Day 10 - File Handling

---
+++ Day10/append_file.py
@@ -0,0 +1,7 @@
+file = open("notes.txt", "a")
+
+file.write("\nI am learning Generative AI.")
+
+file.close()
+
+print("Data Added")
---
+++ Day10/write_file.py
@@ -0,0 +1,8 @@
+file = open("notes.txt", "w")
+
+file.write("Hello Eiman!\n")
+file.write("Welcome to File Handling.")
+
+file.close()
+
+print("File Created Successfully")
